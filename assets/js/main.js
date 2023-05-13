2a8ad4d698cd046a2f62aaef5c5d8a5d198a51f5
Rui Machado <102834545+let-RMXX@users.noreply.github.com>
2023-05-13 15:59:43 +0100

    Skills dropdown menu finish
    Finished skills section and the dropdown menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,7 +30,23 @@ function linkAction(){
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*==================== ACCORDION SKILLS ====================*/
+const skillsContent = document.getElementsByClassName('skills__content'),
+    skillsHeader = document.querySelectorAll('.skills__header')
 
+function dropdownSkills(){
+    let itemClass = this.parentNode.className
+
+    for (i = 0; i < skillsContent.length; i++){
+        skillsContent[i].className = 'skills__content skills__close'
+    }
+    if(itemClass === 'skills__content skills__close'){
+        this.parentNode.className = 'skills__content skills__dropdown'
+    }
+}
+
+skillsHeader.forEach((el) =>{
+    el.addEventListener('click', dropdownSkills)
+})
 
 /*==================== QUALIFICATION TABS ====================*/
 
@@ -39,9 +55,6 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 
 
 /*==================== PORTFOLIO SWIPER  ====================*/
-
-
-/*==================== TESTIMONIAL ====================*/
 
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
